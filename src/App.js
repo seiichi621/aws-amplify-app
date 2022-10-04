cbd35cc7cea000a36eef0706bac7a47384d91af8
github seiichi621 <on88.seiichi.miyajima@gmail.com>
2022-10-04 21:31:36 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { useRef, useState } from "react";
 import './App.css';
+import ImageGrallery from './ImageGrallery';
 
 function App() {
+  const [fetchData, setFetchData] = useState([]);
+  const ref = useRef();
+
+  const hadleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+
+    //APIURL
+    const endpointURL = 
+    `https://pixabay.com/api/?key=29699986-34c552a0800611e4cefed7445&q=${ref.current.value}&image_type=写真`
+    //APIを叩く（データフェエッチング
+    fetch(endpointURL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data.hits);
+        setFetchData(data.hits);
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>My Pixabay</h2>
+      <form onSubmit={(e) => hadleSubmit(e)}>
+        <input type="text" placeholder="画像を探す" ref={ref}></input>
+      </form>
+    <ImageGrallery fetchData={fetchData}/>
     </div>
-  );
-}
+  )
+} 
 
 export default App;
